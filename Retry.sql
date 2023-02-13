d749cf078ad758445fae02a66048e34fd6bb23d6
HOsthzn <110474099+HOsthzn@users.noreply.github.com>
2023-02-13 12:14:02 +0200

    Update Retry.sql

--- Retry.sql
+++ Retry.sql
@@ -1,4 +1,4 @@
-CREATE OR ALTER PROCEDURE Retry (@TSQLCode NVARCHAR(MAX), @RetryCount INT, @IntervalTime VARCHAR(8))
+CREATE OR ALTER PROCEDURE Retry(@TSQLCode NVARCHAR(MAX), @RetryCount INT, @IntervalTime VARCHAR(8))
 AS
 BEGIN
     -- Declare a variable to keep track of the number of retries left
@@ -12,33 +12,36 @@ BEGIN
 
     -- Start a loop that will repeat the execution of the SQL code for the specified number of retries
     WHILE (@retry > 0)
-    BEGIN
-        BEGIN TRY
-            -- Set the SQL variable to the given SQL code
-            SET @SQL = N'EXEC sp_executesql @TSQLCode, N''@TSQLCode NVARCHAR(MAX)'', @TSQLCode';
+        BEGIN
+            BEGIN TRY
+                -- The following code looks strange and redundant, It's not. 
+                -- This code parameterizes and preventing sql injection
 
-            -- Execute the SQL code using sp_executesql
-            EXEC sp_executesql @SQL, N'@TSQLCode NVARCHAR(MAX)', @TSQLCode;
+                -- Set the SQL variable to the given SQL code
+                SET @SQL = N'EXEC sp_executesql @TSQLCode, N''@TSQLCode NVARCHAR(MAX)'', @TSQLCode';
 
-            -- If the execution of the SQL code was successful, set the number of retries to 0 to exit the loop
-            SET @retry = 0;
-        END TRY
-        BEGIN CATCH
-            -- If the execution of the SQL code fails, wait for the specified interval time before retrying
-            WAITFOR DELAY @interval;
+                -- Execute the SQL code using sp_executesql
+                EXEC sp_executesql @SQL, N'@TSQLCode NVARCHAR(MAX)', @TSQLCode;
 
-            -- Print the number of retries left
-            PRINT @retry;
+                -- If the execution of the SQL code was successful, set the number of retries to 0 to exit the loop
+                SET @retry = 0;
+            END TRY
+            BEGIN CATCH
+                -- If the execution of the SQL code fails, wait for the specified interval time before retrying
+                WAITFOR DELAY @interval;
 
-            -- Decrement the number of retries left
-            SET @retry -= 1;
+                -- Print the number of retries left
+                PRINT @retry;
 
-            -- If there are no more retries left, raise an error with the error message
-            IF (@retry <= 0)
-            BEGIN
-                DECLARE @ErrorMessage NVARCHAR(4000) = ERROR_MESSAGE();
-                RAISERROR (@ErrorMessage, 16, 1);
-            END
-        END CATCH
-    END
+                -- Decrement the number of retries left
+                SET @retry -= 1;
+
+                -- If there are no more retries left, raise an error with the error message
+                IF (@retry <= 0)
+                    BEGIN
+                        DECLARE @ErrorMessage NVARCHAR(4000) = ERROR_MESSAGE();
+                        RAISERROR (@ErrorMessage, 16, 1);
+                    END
+            END CATCH
+        END
 END
