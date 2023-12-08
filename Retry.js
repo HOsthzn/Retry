817112b4e5db126e3ffa6fd03b1fe6116615e04b
Hendrik Oosthuizen <110474099+HOsthzn@users.noreply.github.com>
2023-12-08 08:04:46 +0200

    Update Retry.js

--- Retry.js
+++ Retry.js
@@ -1,61 +1,95 @@
+// The Retry object contains various strategies for implementing retries
 const Retry = {
-    // Define the retry strategies.
+    // Strategy object for different retry back-off mechanisms
     Strategy: {
-        FIXED_INTERVAL:      "FixedInterval",
-        EXPONENTIAL_BACKOFF: "ExponentialBackOff"
+        // Each retry waits for a fixed amount of time
+        FIXED_INTERVAL: "FixedInterval",
+        // The wait time for the next retry doubles after each attempt
+        EXPONENTIAL_BACKOFF: "ExponentialBackOff",
+        // The wait time is randomly selected for each retry
+        RANDOM: "Random",
+        // The wait time increases incrementally after each attempt
+        INCREMENTAL: "Incremental",
+        // The wait time follows the Fibonacci sequence after each attempt
+        FIBONACCI: "Fibonacci",
+        // The wait times are part of a progression series
+        PROGRESSION: "Progression",
+        // Wait times between retries are jittered to spread out the load
+        JITTER: "Jitter",
+        // The wait times follows a gamma distribution
+        GAMMA: "Gamma",
     },
-    // Method to get the retry interval based on the retry strategy.
+    // Function to calculate the retry interval based on the strategy used
     getRetryInterval: (attempted, retryInterval, strategy) => {
-        // If retry strategy is Exponential Backoff, retry interval exponentially increases with each attempt.
-        // Otherwise, retry interval is constant.
-        return strategy === Retry.Strategy.EXPONENTIAL_BACKOFF
-               ? retryInterval * Math.pow(2, attempted)
-               : retryInterval;
+        switch (strategy) {
+            case Retry.Strategy.EXPONENTIAL_BACKOFF:
+                return retryInterval * Math.pow(2, attempted);
+            case Retry.Strategy.RANDOM:
+                return Math.floor(Math.random() * (retryInterval * Math.pow(2, attempted)));
+            case Retry.Strategy.INCREMENTAL:
+                return retryInterval * attempted;
+            case Retry.Strategy.FIBONACCI:
+                const fib = (function () {
+                    let fibCache = [0, 1];
+                    return function (n) {
+                        if (fibCache[n] === undefined) {
+                            fibCache[n] = fib(n - 1) + fib(n - 2);
+                        }
+                        return fibCache[n];
+                    }
+                })();
+                return retryInterval * fib(attempted);
+            case Retry.Strategy.PROGRESSION:
+                return retryInterval + (attempted * attempted);
+            case Retry.Strategy.JITTER:
+                return retryInterval * Math.pow(2, attempted) * Math.random();
+            case Retry.Strategy.GAMMA:
+                return retryInterval * (attempted * Math.log(attempted));
+            default:
+                return retryInterval;
+        }
     },
-    // Method to retry a synchronous action.
-    attempt: (action, retryInterval, maxAttempts = 3, strategy = Retry.Strategy.FIXED_INTERVAL) => {
+    // Function to attempt the provided action and retry if necessary using the prescribed strategy
+    attempt: (action, retryInterval, maxAttempts = 3, strategy = Retry.Strategy.FIXED_INTERVAL,
+              logger                                           = console.log) => {
+        // Array to store any exceptions encountered during attempt
         let exceptions = [];
-        // Retry the action until max attempts is reached.
         for (let attempted = 0; attempted < maxAttempts; attempted++) {
             try {
-                action();  // Perform the action.
-                return;    // If it's successful, exit from the function.
+                return action(); // If successful, return the result of the action.
             } catch (ex) {
-                exceptions.push(ex); // In case of exception, store exception and retry.
-                // If all attempts fail, log exceptions.
+                exceptions.push(ex);
                 if (attempted === maxAttempts - 1) {
-                    console.log('All attempts failed. Exceptions:', exceptions);
+                    logger('All attempts failed. Exceptions:', exceptions);
+                    throw ex; // If all attempts fail, throw the latest exception.
                 }
             }
         }
     },
-    // Method to retry an asynchronous action.
-    attemptAsync: async (action, retryInterval, maxAttempts = 3, strategy = Retry.Strategy.FIXED_INTERVAL) => {
+    // Function to attempt the provided action in a non-blocking(async) manner and retry if necessary using the prescribed strategy
+    attemptAsync: async (action, retryInterval, maxAttempts = 3, strategy = Retry.Strategy.FIXED_INTERVAL,
+                         logger                                           = console.log) => {
+        // Array to store any exceptions encountered during attempt
         let exceptions = [];
-        // Retry the action until max attempts is reached.
         for (let attempted = 0; attempted < maxAttempts; attempted++) {
             try {
-                // If not first attempt, wait before retrying.
                 if (attempted > 0) {
+                    // If this is not the first attempt, wait for a certain duration based on the retry strategy
                     await new Promise(
                         resolve => setTimeout(resolve, Retry.getRetryInterval(attempted, retryInterval, strategy)));
                 }
-                await action(); // Perform the action.
-                return;         // If it's successful, exit from the function.
+                return await action(); // If successful, return the result of the action.
             } catch (ex) {
-                exceptions.push(ex); // In case of exception, store exception and retry.
-                // If all attempts fail, log exceptions and throw an error.
+                exceptions.push(ex);
                 if (attempted === maxAttempts - 1) {
-                    console.log('All attempts failed. Exceptions:', exceptions);
+                    logger('All attempts failed. Exceptions:', exceptions);
+                    // If all attempts fail, throw a new error stating the failure.
                     throw new Error(`All attempts failed after ${maxAttempts} retries`);
                 }
                 else {
-                    // In development environment, log the exceptions for debugging.
-                    if (developmentEnvironment) {
-                        console.log(
-                            `Failed to complete action, retrying in ${Retry.getRetryInterval(attempted, retryInterval,
-                                                                                             strategy)}: ${ex}`);
-                    }
+                    // Log the exception and the time to next retry
+                    logger(`Failed to complete action, retrying in ${Retry.getRetryInterval(attempted, retryInterval,
+                                                                                            strategy)}: ${ex}`);
                 }
             }
         }
